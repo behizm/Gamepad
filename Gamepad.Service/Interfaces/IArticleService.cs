4a3874492d782dc62fd28a5f01fea1cf9459ec69
behizm <behizm@gmail.com>
2016-01-13 15:04:42 +0330

    Make Article Services

--- Gamepad.Service/Interfaces/IArticleService.cs
+++ Gamepad.Service/Interfaces/IArticleService.cs
@@ -1,9 +1,13 @@
-﻿using Gamepad.Service.Data.Entities;
+﻿using System;
+using Gamepad.Service.Data.Entities;
 using Gamepad.Service.Services;
 
 namespace Gamepad.Service.Interfaces
 {
     public interface IArticleService : IBaseService<IArticleService, Article>
     {
+        Article FindByTitle(string title);
+        Article FindByName(string name);
+        string ChangePoster(Guid articleId, Guid posterId);
     }
 }
--- Gamepad.Service/Services/ArticleService.cs
+++ Gamepad.Service/Services/ArticleService.cs
@@ -1,6 +1,9 @@
-﻿using Gamepad.Service.Data;
+﻿using System;
+using Gamepad.Service.Data;
 using Gamepad.Service.Data.Entities;
 using Gamepad.Service.Interfaces;
+using Gamepad.Service.Models.ResultModels;
+using Gamepad.Service.Resources;
 
 namespace Gamepad.Service.Services
 {
@@ -13,6 +16,82 @@ namespace Gamepad.Service.Services
         public IArticleService Clone()
         {
             return new ArticleService(new GamepadContext());
+        }
+
+        public Article FindByTitle(string title)
+        {
+            title = title.ToLower();
+            return Get(x => x.Title == title);
+        }
+
+        public Article FindByName(string name)
+        {
+            name = name.ToLower();
+            return Get(x => x.Name == name);
+        }
+
+        public override OperationResult Insert(Article item)
+        {
+            var title = item.Title.Trim();
+            while (title.Contains("  "))
+            {
+                title = title.Replace("  ", " ");
+            }
+            var name = title.ToLower().Replace(" ", "_");
+
+            if (FindByName(name) != null)
+            {
+                return OperationResult.Failed(ErrorMessages.Services_General_Duplicate);
+            }
+
+            item.Title = title;
+            item.Name = name;
+            item.EditDate = null;
+            item.UserScoresAverage = null;
+            return base.Insert(item);
+        }
+
+        public override OperationResult Update(Article item)
+        {
+            var title = item.Title.Trim();
+            while (title.Contains("  "))
+            {
+                title = title.Replace("  ", " ");
+            }
+            var name = title.ToLower().Replace(" ", "_");
+            var article = FindByName(name);
+            if (article.Id != item.Id)
+            {
+                return OperationResult.Failed(ErrorMessages.Services_General_Duplicate);
+            }
+
+            item.Title = title;
+            item.Name = name;
+            item.EditDate = DateTime.Now;
+            return base.Update(item);
+        }
+
+        public string ChangePoster(Guid articleId, Guid posterId)
+        {
+            var article = FindById(articleId);
+            if (article == null)
+            {
+                return null;
+            }
+
+            var file = GpServices.File.FindById(posterId);
+            if (file == null)
+            {
+                return null;
+            }
+
+            if (article.PosterId == file.Id)
+            {
+                return file.Address;
+            }
+            article.PosterId = file.Id;
+            article.EditDate = DateTime.Now;
+            return base.Update(article).Succeeded ? file.Address : null;
         }
     }
 }
